d9a36476ceef3d40aa36078721abeb2b55506abb
pmmuthukumarofficial <82325843+pmmuthukumarofficial@users.noreply.github.com>
2025-02-03 00:36:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use('', bookRouter)
 
 app.listen('5000', ()=>console.log("server running on 5000"))
 
-mongoose.connect('mongodb://localhost:27017/sample')
+mongoose.connect('mongodb+srv://pmmuthukumarofficial:Muthu123@sample.dk2hn.mongodb.net/?retryWrites=true&w=majority&appName=sample')
     .then(()=>console.log('database connected'))
     .catch((err)=> console.log(err.message))
 
